0e9cf41891700d6d64ba1ad1816db7cc9cb4b43d
James Lee <jameslee.98331@gmail.com>
2023-08-03 17:51:42 +0100

    update file

--- Research/single_component/2d_skew_norm_single.jl
+++ Research/single_component/2d_skew_norm_single.jl
@@ -6,12 +6,10 @@ addprocs(10)
 
 ns = [100, 250, 500, 750, 1000, 1500, 2500, 5000, 7500, 10000]
 n_sets = 50
-sets = 1:n_sets
+sets = 1:n_sets::Array{Int}
+alphas = [0, 1, 2, 5, 7]::Array{Int}
 
-@everywhere function run_simulation(x)
-    mcmc_its = 10^5
-    mcmc_burn = Int(mcmc_its / 10)
-    t_max = 100
+@everywhere function run_simulation(x, mcmc_its, mcmc_burn, t_max)
     mfm_options = BayesianMixtures.options(
         "MVN",
         "MFM",
@@ -25,28 +23,42 @@ sets = 1:n_sets
     return mfm_result
 end
 
+
 # iterate over sets
 @sync @distributed for set in sets
 
     all_data = h5read(
-        "./data_inputs/single_skew_normal_alpha=7_2d_set-$set.jld",
+        "./data_inputs/single_skew_normal_alpha=1_2d_set-$set.jld",
         "data"
     )
 
     for n in ns
+        for alpha in alphas
 
-        # create dataset with 1 single component
-        data = [all_data[j, :]::Array{Float64} for j in 1:n]
+            # create dataset with 1 single component
+            data = [all_data[j, :]::Array{Float64} for j in 1:n]
 
-        # run MFM sampler
-        result = run_simulation(data)
-        k_posterior = BayesianMixtures.k_posterior(result)
+            # run MFM sampler
+            mcmc_its = 10^5
+            mcmc_burn = Int(mcmc_its / 10)
+            t_max = Int(n / 2)
+            result = run_simulation(data, mcmc_its, mcmc_burn, t_max)
 
-        # what results to store
-        save(
-            "./comp_outputs/k_posterior_single_skew_normal_alpha=7_2d_n=$n-set-$set.jld",
-            "k_posterior",
-            k_posterior
-        )
+            k_posterior = BayesianMixtures.k_posterior(result)
+            t_posterior = BayesianMixtures.t_posterior(result)
+
+            # what results to store
+            save(
+                "./comp_outputs/k_posterior_single_skew_normal_2d-n=$n-alpha=$alpha-set-$set.jld",
+                "k_posterior",
+                k_posterior
+            )
+
+            save(
+                "./comp_outputs/t_posterior_single_skew_normal_2d-n=$n-alpha=$alpha-set-$set.jld",
+                "t_posterior",
+                t_posterior
+            )
+        end
     end
 end
