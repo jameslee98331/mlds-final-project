e1027cdafab4e1370cfb0c7ea71b4ee0ce802859
James Lee <jameslee.98331@gmail.com>
2023-09-08 18:59:12 +0100

    Tidy repo

--- Research/base_cases/gaussian_mixtures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#PBS -N gaussian_mixtures
-#PBS -m be
-#PBS -q large256
-
-cd /home/ma/w/wkl16/coding/mlds-final-project/Research/base_cases
-/usr/local/bin/julia < gaussian_mixtures.jl > gaussian_mixtures_sim.out
--- Research/base_cases/laplace_mixtures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#PBS -N laplace_mixtures
-#PBS -m be
-#PBS -q large256
-
-cd /home/ma/w/wkl16/coding/mlds-final-project/Research/base_cases
-/usr/local/bin/julia < laplace_mixtures.jl > laplace_mixtures_sim.out
